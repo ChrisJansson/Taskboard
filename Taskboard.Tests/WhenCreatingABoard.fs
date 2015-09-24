9a696ae388d2d3bb0b27756ecd48ade9cc710461
Chris Jansson <chris.janssom@gmail.com>
2015-09-24 09:56:40 +0200

    Create taskboard

---
+++ Taskboard.Tests/WhenCreatingABoard.fs
@@ -0,0 +1,23 @@
+﻿module ``When creating a board``
+
+open Xunit
+open Swensen.Unquote
+open System
+
+let Given events =
+    List.fold (fun s e -> evolve e s) Board.initial events
+
+[<Fact>]
+let ``Board should be created for initial state``() =
+    let state = Given []
+    let command = CreateBoardCommand { Id = BoardId 1; Name = "New board name"; }
+    let producedEvents = handle command state
+    let expectedEvents = [ { BoardCreated.Id = BoardId 1; Name = "New board name"; } ]
+    test <@ expectedEvents = producedEvents @>
+
+[<Fact>]
+let ``Board can not be recreated``() =
+    let state = Given [ BoardCreated { Id = BoardId 1; Name = "New board name"; } ]
+    let command = CreateBoardCommand { Id = BoardId 1; Name = "Create again"; }
+    let act () = handle command state |> ignore
+    Assert.Throws<System.Exception>(Action act)
---
+++ Taskboard/Domain.fs
@@ -0,0 +1,65 @@
+﻿[<AutoOpen>]
+module Domain
+
+
+type CardId = CardId of int
+
+type Card = 
+    {
+        Id : CardId
+    }
+
+type ColumnId = ColumnId of int
+
+type Column = 
+    {
+        Id : ColumnId
+        Name : string
+        Cards : Card list
+    }
+    
+type BoardId = BoardId of int
+
+type Board =
+    {
+        Id : BoardId
+        Name : string
+        Columns : Column list
+        Created : bool
+    }
+    with 
+    static member initial = { Id = BoardId 0; Name = "None"; Columns = []; Created = false }
+
+    
+type Command =
+    | CreateBoardCommand of CreateBoardCommand
+
+and CreateBoardCommand = 
+    {
+        Id : BoardId
+        Name : string
+    }
+
+type Event =
+    | BoardCreated of BoardCreated
+
+and BoardCreated =
+    {
+        Id : BoardId
+        Name : string
+    }
+
+let createBoard (command:CreateBoardCommand) state =
+    if not state.Created then
+        [ { BoardCreated.Id = command.Id; Name = command.Name } ]
+    else failwith "The board is already created"
+
+let handle command state  =
+    match command with
+    | CreateBoardCommand createBoardCommand -> createBoard createBoardCommand state
+
+let evolve event state =
+    match event with
+    | BoardCreated bc ->
+        { Id = bc.Id; Name = bc.Name; Columns = []; Created = true }
+        
