cc10c4b181dde00a3652a66c67034547e436330c
Chris Jansson <chris.janssom@gmail.com>
2015-09-30 09:02:57 +0200

    Move cards between columns

--- Taskboard.Web/taskboard.jsx
+++ Taskboard.Web/taskboard.jsx
@@ -16,32 +16,88 @@ var Taskboard = React.createClass({
 		this.setState({data: newBoard});
 		return;
 	},
+	handleMoveCard: function(sourceColumnId, targetColumnId, targetCardId) {
+		if(sourceColumnId === targetColumnId)
+			return;
+	
+		var board = this.state.data;
+		var findColumn = function (columnId) {
+			return function (column) {
+				return column.id === columnId;
+			};
+		};
+	
+		var sourceColumnIndex = board.columns.findIndex(findColumn(sourceColumnId));
+		var targetColumnIndex = board.columns.findIndex(findColumn(targetColumnId));
+		var sourceColumn = board.columns[sourceColumnIndex];
+		var targetColumn = board.columns[targetColumnIndex];
+			
+		var newSourceColumnCards = sourceColumn.cards.filter(function (card) {
+			return card.id !== targetCardId
+		});
+		
+		var newSourceColumn = React.addons.update(sourceColumn, {
+			cards: { $set : newSourceColumnCards }
+		});
+		
+		var card = sourceColumn.cards.find(function (card) {
+			return card.id === targetCardId;
+		});
+		
+		newTargetColumn = React.addons.update(targetColumn, {
+			cards: { $push: [ card ] }
+		});
+				
+		var newBoard = React.addons.update(board, {
+			columns: { 
+				$splice: [[ sourceColumnIndex, 1, newSourceColumn ], [ targetColumnIndex, 1, newTargetColumn ]]
+			 }
+		});
+		
+		this.setState({data: newBoard});
+	},
 	render: function() {
 		var columnNodes = this.state.data.columns.map(function (column) {
 			return (
-				<TaskboardColumn data={column} />
+				<TaskboardColumn data={column} onMoveCard={this.handleMoveCard} />
 			);
-		});
+		}, this);
 		return (
 			<div className="taskboard">
 				<div className="taskboardColumns">
 					{columnNodes}
 				</div>
-				<TaskboardAddColumnForm onColumnCreateSubmit={this.handleColumnCreateSubmit}/>
+				<TaskboardAddColumnForm onColumnCreateSubmit={this.handleColumnCreateSubmit} />
 			</div>
 		);
 	}
 });
 
 var TaskboardColumn = React.createClass({
+	handleOnDragStart: function(e, card) {
+		e.dataTransfer.effectAllowed = "move";
+		var moveCard = {
+			columnId: this.props.data.id,
+			card: card
+		};
+		e.dataTransfer.setData("text", JSON.stringify(moveCard));
+	},
+	handleOnDragOver: function(e) {
+		e.dataTransfer.dropEffect = "move";
+		e.preventDefault();
+	},
+	handleOnDrop: function(e) {
+		var moveCard = JSON.parse(e.dataTransfer.getData("text"));	
+		this.props.onMoveCard(moveCard.columnId, this.props.data.id, moveCard.card.id);
+	},
 	render: function() {
 		var cardNodes = this.props.data.cards.map(function (card) {
 			return (
-				<TaskboardCard data={card} />
+				<TaskboardCard data={card} onCardDragStart={this.handleOnDragStart} onDragStart={this.handleOnDragStart} />
 			);
-		});
+		}, this);
 		return (
-			<div className="taskboardColumn panel panel-default">
+			<div className="taskboardColumn panel panel-default" onDragOver={this.handleOnDragOver} onDrop={this.handleOnDrop}>
 				<div className="panel-heading taskboardColumnHeader">
 					{this.props.data.name}
 				</div>
@@ -54,9 +110,12 @@ var TaskboardColumn = React.createClass({
 });
 
 var TaskboardCard = React.createClass({
+	handleOnDragStart: function(e) {
+		this.props.onDragStart(e, this.props.data);
+	},
 	render: function() {
 		return (
-			<div className="taskboardCard panel panel-primary" draggable="true">
+			<div className="taskboardCard panel panel-primary" draggable="true" onDragStart={this.handleOnDragStart}>
 				<div className="taskboardColumnCardHeader panel-heading">
 					{this.props.data.name}
 				</div>
