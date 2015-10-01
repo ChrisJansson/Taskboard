a6e144c26b044315f456f7c7cbc59da99223e2a6
Chris Jansson <chris.janssom@gmail.com>
2015-10-01 08:49:22 +0200

    Refactored test case

--- Taskboard.Tests/WhenAddingAState.fs
+++ Taskboard.Tests/WhenAddingAState.fs
@@ -5,10 +5,9 @@ open Swensen.Unquote
 
 [<Fact>]
 let ``A state is added to the board`` () =
-    let command = CreateStateCommand { BoardId = BoardId 1; Name = "New state" }
-    let events = Given [ BoardCreated { Id = BoardId 1; Name = "Board"; } ] |> When command
-    let expectedEvents = [ StateCreated { Id = StateId 0; Name = "New state" } ]
-    test<@ events = expectedEvents @>
+    Given [ BoardCreated { Id = BoardId 1; Name = "Board"; } ] 
+    |> When (CreateStateCommand { BoardId = BoardId 1; Name = "New state" })
+    |> Then [ StateCreated { Id = StateId 0; Name = "New state" } ]
 
 [<Fact>]
 let ``Another state is added to the board`` () =
